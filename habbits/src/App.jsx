0fe92b6a7e63c198a547023524d83879141461d3
Lorens <gueronlj@gmail.com>
2023-11-06 17:44:22 -0500

    multiple graphs

--- habbits/src/App.jsx
+++ habbits/src/App.jsx
@@ -10,11 +10,72 @@ const user = {
   habbits: [
     {
       name: 'Gym',
-      count: 0
+      data: [
+        { date: '2024/01/11', count:2 },
+        ...[...Array(20)].map((_, idx) => ({ date: `2024/01/${idx + 1}`, count: idx })),
+        ...[...Array(22)].map((_, idx) => ({ date: `2024/02/${idx + 2}`, count: idx })),
+        ...[...Array(28)].map((_, idx) => ({ date: `2024/03/${idx + 1}`, count: idx })),
+        ...[...Array(27)].map((_, idx) => ({ date: `2024/04/${idx + 3}`, count: idx })),
+        ...[...Array(27)].map((_, idx) => ({ date: `2024/05/${idx + 1}`, count: idx })),
+        ...[...Array(26)].map((_, idx) => ({ date: `2024/06/${idx + 1}`, count: idx })),
+        ...[...Array(19)].map((_, idx) => ({ date: `2024/07/${idx + 5}`, count: idx })),
+        ...[...Array(30)].map((_, idx) => ({ date: `2024/08/${idx + 1}`, count: idx })),
+        ...[...Array(23)].map((_, idx) => ({ date: `2024/09/${idx + 6}`, count: idx })),
+        ...[...Array(26)].map((_, idx) => ({ date: `2024/10/${idx + 1}`, count: idx })),
+        ...[...Array(27)].map((_, idx) => ({ date: `2024/11/${idx + 1}`, count: idx })),
+        { date: '2024/04/12', count:2 },
+        { date: '2024/05/01', count:5 },
+        { date: '2024/05/02', count:5 },
+        { date: '2024/05/03', count:1 },
+        { date: '2024/05/04', count:11 },
+        { date: '2024/05/08', count:32 },
+      ]
     },
     {
       name: 'Coding',
-      count: 0
+      data: [
+        { date: '2024/01/11', count:2 },
+        ...[...Array(20)].map((_, idx) => ({ date: `2024/01/${idx + 1}`, count: idx })),
+        ...[...Array(22)].map((_, idx) => ({ date: `2024/02/${idx + 2}`, count: idx })),
+        ...[...Array(28)].map((_, idx) => ({ date: `2024/03/${idx + 1}`, count: idx })),
+        ...[...Array(27)].map((_, idx) => ({ date: `2024/04/${idx + 3}`, count: idx })),
+        ...[...Array(27)].map((_, idx) => ({ date: `2024/05/${idx + 1}`, count: idx })),
+        ...[...Array(26)].map((_, idx) => ({ date: `2024/06/${idx + 1}`, count: idx })),
+        ...[...Array(19)].map((_, idx) => ({ date: `2024/07/${idx + 5}`, count: idx })),
+        ...[...Array(30)].map((_, idx) => ({ date: `2024/08/${idx + 1}`, count: idx })),
+        ...[...Array(23)].map((_, idx) => ({ date: `2024/09/${idx + 6}`, count: idx })),
+        ...[...Array(26)].map((_, idx) => ({ date: `2024/10/${idx + 1}`, count: idx })),
+        ...[...Array(27)].map((_, idx) => ({ date: `2024/11/${idx + 1}`, count: idx })),
+        { date: '2024/04/12', count:2 },
+        { date: '2024/05/01', count:5 },
+        { date: '2024/05/02', count:5 },
+        { date: '2024/05/03', count:1 },
+        { date: '2024/05/04', count:11 },
+        { date: '2024/05/08', count:32 },
+      ]
+    },
+    {
+      name: 'Guitar Practice',
+      data: [
+        { date: '2024/01/11', count:2 },
+        ...[...Array(20)].map((_, idx) => ({ date: `2024/01/${idx + 1}`, count: idx })),
+        ...[...Array(22)].map((_, idx) => ({ date: `2024/02/${idx + 2}`, count: idx })),
+        ...[...Array(28)].map((_, idx) => ({ date: `2024/03/${idx + 1}`, count: idx })),
+        ...[...Array(27)].map((_, idx) => ({ date: `2024/04/${idx + 3}`, count: idx })),
+        ...[...Array(27)].map((_, idx) => ({ date: `2024/05/${idx + 1}`, count: idx })),
+        ...[...Array(26)].map((_, idx) => ({ date: `2024/06/${idx + 1}`, count: idx })),
+        ...[...Array(19)].map((_, idx) => ({ date: `2024/07/${idx + 5}`, count: idx })),
+        ...[...Array(30)].map((_, idx) => ({ date: `2024/08/${idx + 1}`, count: idx })),
+        ...[...Array(23)].map((_, idx) => ({ date: `2024/09/${idx + 6}`, count: idx })),
+        ...[...Array(26)].map((_, idx) => ({ date: `2024/10/${idx + 1}`, count: idx })),
+        ...[...Array(27)].map((_, idx) => ({ date: `2024/11/${idx + 1}`, count: idx })),
+        { date: '2024/04/12', count:2 },
+        { date: '2024/05/01', count:5 },
+        { date: '2024/05/02', count:5 },
+        { date: '2024/05/03', count:1 },
+        { date: '2024/05/04', count:11 },
+        { date: '2024/05/08', count:32 },
+      ]
     }
   ]
 }
@@ -26,7 +87,19 @@ const menuItems = ['Login', 'Logout', 'Profile'].map((key) => ({
 
 function App() {
   
-  const [heatColors, setHeatColors] = useState();
+  const [currentUser, setCurrentUser] = useState(user);
+
+  const allHeatMaps = currentUser.habbits.map((habbit, i ) => {
+    return (
+      <RHM
+        key={i}
+        activity={habbit.name}
+        data={habbit.data}
+
+      />
+    )
+  })
+
 
   return (
     <div className="App">
@@ -39,8 +112,9 @@ function App() {
           <h2>Happy Habbit</h2>
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={menuItems} />
         </Header>
-
-        <RHM/>
+        <div className='content'>
+          {allHeatMaps}
+        </div>
     </div>
   )
 }
--- habbits/src/components/Heatmap/RHM.jsx
+++ habbits/src/components/Heatmap/RHM.jsx
@@ -3,36 +3,14 @@ import HeatMap from '@uiw/react-heat-map';
 import { Toolbar } from '../Toolbar/toolbar';
 import { ColorSelect } from '../Dropdown/dropdown';
 
-export const RHM = () => {
+export const RHM = ({data, activity}) => {
    const [heatColors, setHeatColors] = useState()
-
-   const value = [
-      { date: '2024/01/11', count:2 },
-      ...[...Array(20)].map((_, idx) => ({ date: `2024/01/${idx + 1}`, count: idx })),
-      ...[...Array(22)].map((_, idx) => ({ date: `2024/02/${idx + 2}`, count: idx })),
-      ...[...Array(28)].map((_, idx) => ({ date: `2024/03/${idx + 1}`, count: idx })),
-      ...[...Array(27)].map((_, idx) => ({ date: `2024/04/${idx + 3}`, count: idx })),
-      ...[...Array(27)].map((_, idx) => ({ date: `2024/05/${idx + 1}`, count: idx })),
-      ...[...Array(26)].map((_, idx) => ({ date: `2024/06/${idx + 1}`, count: idx })),
-      ...[...Array(19)].map((_, idx) => ({ date: `2024/07/${idx + 5}`, count: idx })),
-      ...[...Array(30)].map((_, idx) => ({ date: `2024/08/${idx + 1}`, count: idx })),
-      ...[...Array(23)].map((_, idx) => ({ date: `2024/09/${idx + 6}`, count: idx })),
-      ...[...Array(26)].map((_, idx) => ({ date: `2024/10/${idx + 1}`, count: idx })),
-      ...[...Array(27)].map((_, idx) => ({ date: `2024/11/${idx + 1}`, count: idx })),
-      { date: '2024/04/12', count:2 },
-      { date: '2024/05/01', count:5 },
-      { date: '2024/05/02', count:5 },
-      { date: '2024/05/03', count:1 },
-      { date: '2024/05/04', count:11 },
-      { date: '2024/05/08', count:32 },
-   ];
-
-   console.log(value)
    
    return (
       <div>
+         <h2>{activity}</h2>
          <HeatMap
-            value={value}
+            value={data}
             rectSize={20}
             weekLabels={['S', 'M', 'T', 'W', 'Th', 'F', 'S']}
             monthLabels={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov','Dec']}
