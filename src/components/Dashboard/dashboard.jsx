55ef2812b6b8676dbccf5a7f1c1fbdf50a859eda
Lorens <gueronlj@gmail.com>
2023-11-18 11:07:44 -0500

    profile, habit delete buttons

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -1,9 +1,30 @@
 import styles from './dashboard.module.css';
+import { useQuery } from '@tanstack/react-query';
+import { fetchHabits } from '../../utilities/api';
 
 const Dashboard = () => {
+   const userId = 1;
+
+   const query = useQuery({queryKey:['habits'], queryFn: () => fetchHabits(userId)})
+
+
+   const handleDelete = (id) => {
+      console.log(`deleting: ${id}`);
+   }
+
    return(
       <div className={styles.container}>
-         <h1>Profile</h1>
+         <h2>Tracked Habits</h2>
+         <div className={styles.cardsContainer}>
+            {query.data?.map((habit) => {
+               return(
+                  <div key={habit.id} className={styles.card}>
+                     <h3>{habit.title}</h3>
+                     <button onClick={() => handleDelete(habit.id)}>Delete</button>
+                  </div>
+               )
+            })}
+         </div>
       </div>
    )
 }
